a0d10c394b0353c022d4f2691ce760d5d898d1f9
Mmx <1624045573@qq.com>
2021-06-22 11:47:09 +0800

    feat:cookie包集成优化

--- cookie.go
+++ cookie.go
@@ -9,16 +9,22 @@ type cookie struct{}
 var Cookie cookie
 
 // Decode 从header中解析cookie进map
-func (*cookie) Decode(a string) map[string]string {
-	var t = make(map[string]string)
+func (*cookie) Decode(a string, o map[string]string) map[string]string {
+	var t map[string]string
+	if o != nil {
+		t = o
+	} else {
+		t = make(map[string]string)
+	}
 	for _, v := range strings.Split(a, ";") {
 		tt := strings.Split(strings.TrimSpace(v), "=")
 		switch tt[0] { //滤去一些杂质
 		case "path":
 		case "HttpOnly":
+		case "SameSite":
 		case "":
 		default:
-			t[tt[0]] = strings.TrimSpace(tt[1])
+			t[tt[0]] = strings.TrimSpace(strings.Join(tt[1:], ""))
 		}
 	}
 	return t
