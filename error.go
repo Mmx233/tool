2159a563c0fc97503318d5725aadd3cccffd41e0
Mmx233 <admin@mmxblog.com>
2023-08-21 23:32:41 +0800

    chore: Recover 函数返回 recover 状态

--- error.go
+++ error.go
@@ -5,10 +5,12 @@ import (
 	"runtime"
 )
 
-func Recover() {
-	if p := recover(); p != nil {
+func Recover() any {
+	var p any
+	if p = recover(); p != nil {
 		fmt.Println(p)
 		var buf [4096]byte
 		fmt.Printf(string(buf[:runtime.Stack(buf[:], false)]))
 	}
+	return p
 }
