7b23d7322df38d2f4931961f657fb4f7d744151b
Mmx233 <admin@mmxblog.com>
2023-12-04 17:22:27 +0800

    feat: rand 工具支持指定字符串随机内容

--- rand.go
+++ rand.go
@@ -5,27 +5,49 @@ import (
 	"unsafe"
 )
 
-const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
-// RandNum [min,max]
-func RandNum(rand *rand.Rand, Min int, Max int) int {
-	return Min + rand.Intn(Max-Min+1)
-}
-
 const (
 	letterIdBits = 6
 	letterIdMask = 1<<letterIdBits - 1
 	letterIdMax  = 63 / letterIdBits
 )
 
-func RandString(src rand.Source, Len int) string {
+type Rand struct {
+	source rand.Source
+	rand   *rand.Rand
+}
+
+func NewRand(src rand.Source) Rand {
+	return Rand{
+		source: src,
+		rand:   rand.New(src),
+	}
+}
+
+// Num [min,max]
+func (r Rand) Num(Min, Max int) int {
+	return Min + rand.Intn(Max-Min+1)
+}
+
+func (r Rand) WithLetters(letters string) RandWithLetters {
+	return RandWithLetters{
+		Rand:    r,
+		letters: letters,
+	}
+}
+
+type RandWithLetters struct {
+	Rand
+	letters string
+}
+
+func (r RandWithLetters) String(Len int) string {
 	b := make([]byte, Len)
-	for i, cache, remain := Len-1, src.Int63(), letterIdMax; i >= 0; {
+	for i, cache, remain := Len-1, r.source.Int63(), letterIdMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = src.Int63(), letterIdMax
+			cache, remain = r.source.Int63(), letterIdMax
 		}
-		if idx := int(cache & letterIdMask); idx < len(letters) {
-			b[i] = letters[idx]
+		if idx := int(cache & letterIdMask); idx < len(r.letters) {
+			b[i] = r.letters[idx]
 			i--
 		}
 		cache >>= letterIdBits
